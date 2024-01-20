dda4f87cd748526e138bf61a2bc623f797c52b5f
tom hikari <tomprescott1565@gmail.com>
2024-01-20 12:35:12 +0100

    Refactor categories component to display products

--- src/app/categories/categories.component.ts
+++ src/app/categories/categories.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ICategory, IProduct, ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
-  styleUrl: './categories.component.scss'
+  styleUrl: './categories.component.scss',
 })
 export class CategoriesComponent {
+  categories: ICategory[] = [];
+  products: IProduct[] = [];
 
+  constructor(
+    private product: ProductService,
+    private router: Router,
+    private routeActive: ActivatedRoute
+  ) {
+    this.categories = this.product.categories;
+    this.products = this.product.products;
+  }
 }
