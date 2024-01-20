e840584f23c0194b3c54bfaf26093cb88e4e5733
tom hikari <tomprescott1565@gmail.com>
2024-01-20 08:45:09 +0100

    Add new components and update angular.json

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { StoreComponent } from './store/store.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ProductComponent } from './product/product.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    AboutComponent,
+    StoreComponent,
+    CategoriesComponent,
+    ProductComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
