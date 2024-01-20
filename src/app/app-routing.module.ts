7314bb11d5a4ba7be45266d526c6b55878a9ddda
tom hikari <tomprescott1565@gmail.com>
2024-01-20 16:44:29 +0100

    Update productResolver to productsResolver

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -45,7 +45,7 @@ const routes: Routes = [
       {
         path: '',
         component: CategoriesComponent,
-        resolve: { products: productResolver },
+        resolve: { products: productsResolver },
       },
       {
         path: ':category/:id',
